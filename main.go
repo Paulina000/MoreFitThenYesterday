f7883a37b04becba80f24ecc7e48e7350bdf78eb
Paulina000 <p.jalosinska@gmail.com>
2025-03-09 21:11:00 +0100

    added Fat % measure function

--- main.go
+++ main.go
@@ -54,7 +54,7 @@ func findGender() string {
 
 }
 
-func findAge() int {
+func findAge() float64 {
 	reader := bufio.NewReader(os.Stdin)
 
 	for {
@@ -79,7 +79,37 @@ func findAge() int {
 
 		// Calculate and return the age if the year is valid
 		age := 2025 - yearInt
-		return age
+		ageFloat := float64(age)
+
+		return ageFloat
+	}
+}
+
+func findMeasureFat() float64 {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		// Ask for a birth year
+		fmt.Println("Give me fat measures in mm:")
+		measureFatmm, _ := reader.ReadString('\n')
+		measureFatmm = measureFatmm[:len(measureFatmm)-1] // Remove the newline character
+		// Check if the input is a valid number
+		measureFatmm = strings.TrimSpace(measureFatmm)
+		if _, err := strconv.ParseFloat(measureFatmm, 64); err != nil {
+			fmt.Println("That's not a valid number. Please try again.")
+			continue
+		}
+		// Convert year to int for further checks
+		bellyFatmmInt, _ := strconv.ParseFloat(measureFatmm, 64)
+
+		// Check if the year is within a reasonable range
+		if bellyFatmmInt < 5 || bellyFatmmInt > 200 {
+			fmt.Println("That's not a reasonable measure. Please try again.")
+			continue
+		}
+
+		// Calculate and return the age if the year is valid
+		return bellyFatmmInt
 	}
 }
 
@@ -88,7 +118,19 @@ func CreateConstantFeatures() {
 	fmt.Println("gender: ", gender)
 	age := findAge()
 	fmt.Println("age: ", age)
+	// backFat := findMeasureFat()
 
+	// fmt.Println("backFat: ", backFat)
+
+	// armFat := findMeasureFat()
+	// fmt.Println("armFat: ", armFat)
+
+	bellyFat := findMeasureFat()
+	fmt.Println("findBellyFat: ", bellyFat)
+	bodyFatWoman := bellyFat*1.223 - (0.0174 * bellyFat * bellyFat) + 0.124*(age-6.07)
+	bodyFatMan := bellyFat*1.378 - (0.0174 * bellyFat * bellyFat) + 0.213*(age-5.84)
+	fmt.Println("bodyFatWoman : ", bodyFatWoman)
+	fmt.Println("bodyFatMan : ", bodyFatMan)
 }
 func main() {
 	CreateConstantFeatures()
